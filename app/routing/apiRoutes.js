e8f616afd35a3260d34a2926eda9f1dff2839405
[eddmoura] <edd.alca@gmail.com>
2017-06-29 17:36:14 -0400

    update1

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+var friendsData = require("../data/friends.js");
+var path = require("path");
+
+module.exports = function(app){
+
+app.get("/api/friends.js", function(req, res) {
+    res.json(friendsData);
+  });
+
+app.post("/api/friends.js", function(req, res){
+  friendsData.push(req.body);
+res.json(true);
+
+});
+
+};
